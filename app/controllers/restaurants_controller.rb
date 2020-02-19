160feb54687cb3e4c224b0a93a3d42ae5cd06d81
Antoine Juteau <gunterstrikedev@gmail.com>
2020-02-19 17:16:43 +0100

    Ex2 Hard Finish

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-
+before_action :set_restaurant, only: [:show]
   def index
     @restaurants = Restaurant.all
   end
@@ -8,9 +8,27 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def show
+  end
+
   def create
-    @restaurant = Restaurant.new(params[:restaurant])
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant)
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
+
+   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :phone_number, :category)
     end
 end
+
